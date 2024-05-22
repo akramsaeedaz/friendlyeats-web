f5b8ebecb1d458288bda8964d4c43a6a4a6e1027
akramsaeedaz <67917782+akramsaeedaz@users.noreply.github.com>
2024-05-22 14:47:31 -0300

    edit code

--- vanilla-js/scripts/FriendlyEats.Data.js
+++ vanilla-js/scripts/FriendlyEats.Data.js
@@ -22,14 +22,14 @@ FriendlyEats.prototype.addRestaurant = function(data) {
   /*
     TODO: Implement adding a document
   */
-FriendlyEats.prototype.getAllRestaurants = function(renderer) {
-  var query = firebase.firestore()
-      .collection('restaurants')
-      .orderBy('avgRating', 'desc')
-      .limit(50);
-
-  this.getDocumentsInQuery(query, renderer);
-};
+    FriendlyEats.prototype.getAllRestaurants = function(renderer) {
+      var query = firebase.firestore()
+          .collection('restaurants')
+          .orderBy('avgRating', 'desc')
+          .limit(50);
+    
+      this.getDocumentsInQuery(query, renderer);
+    };
   /*
     TODO: Retrieve list of restaurants
   */
@@ -61,29 +61,29 @@ FriendlyEats.prototype.getRestaurant = function(id) {
   */
 
 
-FriendlyEats.prototype.getFilteredRestaurants = function(filters, renderer) {
-  var query = firebase.firestore().collection('restaurants');
-
-  if (filters.category !== 'Any') {
-    query = query.where('category', '==', filters.category);
-  }
-
-  if (filters.city !== 'Any') {
-    query = query.where('city', '==', filters.city);
-  }
-
-  if (filters.price !== 'Any') {
-    query = query.where('price', '==', filters.price.length);
-  }
-
-  if (filters.sort === 'Rating') {
-    query = query.orderBy('avgRating', 'desc');
-  } else if (filters.sort === 'Reviews') {
-    query = query.orderBy('numRatings', 'desc');
-  }
-
-  this.getDocumentsInQuery(query, renderer);
-};
+    FriendlyEats.prototype.getFilteredRestaurants = function(filters, renderer) {
+      var query = firebase.firestore().collection('restaurants');
+    
+      if (filters.category !== 'Any') {
+        query = query.where('category', '==', filters.category);
+      }
+    
+      if (filters.city !== 'Any') {
+        query = query.where('city', '==', filters.city);
+      }
+    
+      if (filters.price !== 'Any') {
+        query = query.where('price', '==', filters.price.length);
+      }
+    
+      if (filters.sort === 'Rating') {
+        query = query.orderBy('avgRating', 'desc');
+      } else if (filters.sort === 'Reviews') {
+        query = query.orderBy('numRatings', 'desc');
+      }
+    
+      this.getDocumentsInQuery(query, renderer);
+    };
   /*
     TODO: Retrieve filtered list of restaurants
   */
